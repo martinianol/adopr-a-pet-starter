d865da1cb6fcead71f748f36f56f0506c7304763
Mars <martinianol@outlook.com>
2021-09-23 12:22:11 -0300

    Links, NavLinks & URLParams added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navigation />
-      <HomePage />
-    </div>
+      <Route path="/:type/:id">
+        <PetDetailsPage />
+      </Route>
+      <Route path="/:type?">
+        <HomePage />
+      </Route>
+    </Router>
   );
 }
 
